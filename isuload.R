945f4eafb0e67125775a847f50f0b738c63c288f
Heike Hofmann <hofmann@iastate.edu>
2023-04-23 16:53:03 -0500

    no dplyr

--- isuload.R
+++ isuload.R
@@ -1,5 +1,6 @@
 library(rvest)
 
 doc <- read_html("https://www.police.iastate.edu/crime-log/")
-pdf_file <- doc %>% html_element(".log") %>% html_attr("href")
+element <- html_element(doc, ".log")
+pdf_file <- html_attr(element, "href")
 download.file(pdf_file, destfile = file.path("isulogs", basename(pdf_file)))
